52a45316f3c20738f1d3377e2fc05a76e029b370
luiz <luizyfs@gmail.com>
2023-07-23 11:41:45 -0300

    2º commit

---
+++ write/src/main/java/io/github/luizyokoyama/write/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package io.github.luizyokoyama.write.config;
+
+
+import com.c4_soft.springaddons.security.oauth2.OAuthentication;
+import com.c4_soft.springaddons.security.oauth2.OpenidClaimSet;
+import com.c4_soft.springaddons.security.oauth2.config.synchronised.OAuth2AuthenticationFactory;
+import com.c4_soft.springaddons.security.oauth2.config.synchronised.ResourceServerExpressionInterceptUrlRegistryPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import java.util.Collection;
+import java.util.Map;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    OAuth2AuthenticationFactory authenticationFactory(Converter<Map<String, Object>, Collection<? extends GrantedAuthority>> authoritiesConverter) {
+        return (bearerString, claims) -> new OAuthentication<>(new OpenidClaimSet(claims),
+                authoritiesConverter.convert(claims), bearerString);
+    }
+
+    @Bean
+    ResourceServerExpressionInterceptUrlRegistryPostProcessor expressionInterceptUrlRegistryPostProcessor() {
+        return (AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) -> registry
+                .requestMatchers(HttpMethod.POST, "/write/**").hasRole("write")
+                .anyRequest().permitAll();//.authenticated();
+
+    }
+
+}
